6d6ff4f43940611d47aebd043c208d25ffa5e0cc
ljl <959566720@qq.com>
2024-04-21 23:27:31 +0800

    init

---
+++ src/main/java/com/yongyuan/order/controller/OrderController.java
@@ -0,0 +1,19 @@
+package com.yongyuan.order.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/order")
+public class OrderController {
+
+    @GetMapping("/test")
+
+    public String test(){
+        return "index.html";
+    }
+
+}
